a4ed55445ce145942d7d6c84572992a8e5179703
downheartedcount <erasylabdulla20@gmail.com>
2022-07-26 19:31:06 +0600

    Qedu

--- lms/settings.py
+++ lms/settings.py
@@ -22,8 +22,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'b*)$+3)4*uqv@gz0+ly_)(2w9(nyv*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://89.46.34.193', 'http://server.qeduprod.kz']
+ALLOWED_HOSTS = ['q-edu.kz', 'www.q-edu.kz']
+CSRF_TRUSTED_ORIGINS = ['http://89.46.34.193', 'q-edu.kz', 'www.q-edu.kz']
 # Application definitionp
 
 INSTALLED_APPS = [
