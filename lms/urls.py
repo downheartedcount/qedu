08bc9f0e2336c3ed5ef2caccab64f3d00fae8f9e
downheartedcount <erasylabdulla20@gmail.com>
2022-07-31 05:19:14 +0600

    settings

--- lms/urls.py
+++ lms/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('elearn.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    #path('accounts/', include('django.contrib.auth.urls'))
 
 ]
 
